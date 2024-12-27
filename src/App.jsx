f0490b745d2086c1d163eed09a22f58064ab4f73
Vyshniakov Ihor <gramazekavip@gmail.com>
2024-12-28 00:15:14 +0100

    Home page: added search

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material'
+import { Container, TextField, Typography } from '@mui/material'
+import Grid from '@mui/material/Grid2'
 
 import './App.css'
 
@@ -13,20 +14,37 @@ import Header from './components/Header'
 // - Filter by category (comedy/horrors/action)
 // - Searching with caching (via useReducer)
 // - Routing inside app (react-router)
+// ? 'to top' button (https://mui.com/material-ui/react-app-bar/#scrolling)
 
 // * Requirements
 // + Using UI lib (https://mui.com/material-ui/)
 // - Usage of all hooks (useState, useEffect, useRef, useReducer, useContext)
-// - Using API (https://developer.themoviedb.org/)
+// - Using API (https://developer.themoviedb.org/reference/intro/getting-started)
 
 function App() {
     return (
         <>
             <Header />
             <main>
-                <Typography variant="h3" component="h3">
-                    Home page
-                </Typography>
+                <Container maxWidth="md">
+                    <Grid container spacing={2} sx={{ margin: '1rem 0' }}>
+                        <Grid size={12}>
+                            <Typography
+                                variant="h3"
+                                component="h3"
+                                align="center"
+                            >
+                                Search your favorite movie!
+                            </Typography>
+                            <TextField
+                                fullWidth
+                                label="Search..."
+                                id="search"
+                                margin="normal"
+                            />
+                        </Grid>
+                    </Grid>
+                </Container>
             </main>
         </>
     )
