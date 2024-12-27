b855d35da6476eaa726e2e9fdf80b71a6872b209
Vyshniakov Ihor <gramazekavip@gmail.com>
2024-12-27 22:32:50 +0100

    added TODO list

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,21 @@ import Button from '@mui/material/Button'
 
 import './App.css'
 
+// TODO 👇
+// * Pages
+// - Home page (popular/trend movies)
+// - Movie page (description, actors, trailer)
+
+// * Features
+// - Filter by category (comedy/horrors/action)
+// - Searching with caching (via useReducer)
+// - Routing inside app (react-router)
+
+// * Requirements
+// + Using UI lib (https://mui.com/material-ui/)
+// - Usage of all hooks (useState, useEffect, useRef, useReducer, useContext)
+// - Using API (https://developer.themoviedb.org/)
+
 function App() {
     return (
         <>
