1d8b8fbd0f5b2b2f89343e1dc5bb3fda25e990ae
Vyshniakov Ihor <gramazekavip@gmail.com>
2025-01-19 18:45:48 +0100

    New Feature(images): loading - animated preview, bad url - placeholder, good url - animated showing

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,29 +1,43 @@
 import { CardMedia, Skeleton } from '@mui/material'
 import { useState } from 'react'
 
-const Image = ({ title, url, ...props }) => {
+const Image = ({ title, url = '', ...props }) => {
+    const [isLoaded, setIsLoaded] = useState(false)
     const [error, setError] = useState(false)
 
-    if (!url || error) {
+    if (error) {
         return (
             <Skeleton
                 style={{ width: '100%', height: '100%' }}
                 variant="rectangular"
                 animation={false}
-                {...props}
             />
         )
     }
 
     return (
-        <CardMedia
-            component="img"
-            image={`https://image.tmdb.org/t/p/w300${url}`}
-            alt={`${title} poster`}
-            onError={() => setError(true)}
-            style={{ width: '100%', height: '100%' }}
-            {...props}
-        />
+        <>
+            {!isLoaded && (
+                <Skeleton
+                    style={{ width: '100%', height: '100%' }}
+                    variant="rectangular"
+                />
+            )}
+            <CardMedia
+                component="img"
+                image={`https://image.tmdb.org/t/p/w300${url}`}
+                alt={`${title} poster`}
+                onError={() => setError(true)}
+                onLoad={() => setIsLoaded(true)}
+                sx={{
+                    width: '100%',
+                    height: '100%',
+                    opacity: isLoaded ? '1' : '0',
+                    transition: 'opacity .5s linear',
+                }}
+                {...props}
+            />
+        </>
     )
 }
 
