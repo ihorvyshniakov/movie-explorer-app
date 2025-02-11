58a1ea27a8d0f057fd979a71080bee0dfda4c472
Vyshniakov Ihor <gramazekavip@gmail.com>
2025-02-11 21:34:46 +0100

    Responsive: fixed mobile grid skeleton

--- src/pages/Home/MoviesGrid/MoviesGridSkeleton.jsx
+++ src/pages/Home/MoviesGrid/MoviesGridSkeleton.jsx
@@ -9,87 +9,92 @@ import Grid from '@mui/material/Grid2'
 
 const MoviesGridSkeleton = () => {
     return [...new Array(6)].map((el, index) => (
-        <Card
+        <Grid
             key={`skeleton-${index}`}
-            sx={{ width: '100%', maxWidth: 345, height: '100%' }}
+            size={4}
+            display="flex"
+            justifyContent="center"
+            sx={{ width: '100%' }}
         >
-            <CardActionArea
-                sx={{
-                    height: '100%',
-                    display: 'flex',
-                    flexDirection: 'column',
-                }}
-            >
-                <Paper
-                    square={true}
-                    elevation={0}
+            <Card sx={{ width: '100%', maxWidth: 345, height: '100%' }}>
+                <CardActionArea
                     sx={{
-                        overflow: 'hidden',
-                        width: '100%',
-                        height: '400px',
+                        height: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
                     }}
                 >
-                    <Skeleton
-                        style={{ width: '100%', height: '100%' }}
-                        variant="rectangular"
-                        animation="pulse"
-                    />
-                </Paper>
-                <CardContent
-                    sx={{
-                        width: '100%',
-                        display: 'grid',
-                        gridTemplateRows: 'auto auto 1fr',
-                        flexGrow: 1,
-                    }}
-                >
-                    <Skeleton
-                        variant="rounded"
-                        height="4rem"
+                    <Paper
+                        square={true}
+                        elevation={0}
+                        sx={{
+                            overflow: 'hidden',
+                            width: '100%',
+                            height: '400px',
+                        }}
+                    >
+                        <Skeleton
+                            style={{ width: '100%', height: '100%' }}
+                            variant="rectangular"
+                            animation="pulse"
+                        />
+                    </Paper>
+                    <CardContent
                         sx={{
                             width: '100%',
-                            fontSize: '1.5rem',
-                            marginBottom: '0.35em',
+                            display: 'grid',
+                            gridTemplateRows: 'auto auto 1fr',
+                            flexGrow: 1,
                         }}
-                    />
-                    <Skeleton
-                        variant="rounded"
-                        height="80px"
-                        sx={{
-                            marginBottom: '1rem',
-                        }}
-                    />
-
-                    <Grid
-                        container
-                        spacing={1}
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="space-between"
-                        alignSelf="flex-end"
                     >
-                        <Grid>
-                            <Skeleton
-                                variant="rounded"
-                                height="2rem"
-                                sx={{
-                                    width: '6rem',
-                                }}
-                            />
+                        <Skeleton
+                            variant="rounded"
+                            height="4rem"
+                            sx={{
+                                width: '100%',
+                                fontSize: '1.5rem',
+                                marginBottom: '0.35em',
+                            }}
+                        />
+                        <Skeleton
+                            variant="rounded"
+                            height="80px"
+                            sx={{
+                                marginBottom: '1rem',
+                            }}
+                        />
+
+                        <Grid
+                            container
+                            spacing={1}
+                            direction="row"
+                            alignItems="center"
+                            justifyContent="space-between"
+                            alignSelf="flex-end"
+                        >
+                            <Grid>
+                                <Skeleton
+                                    variant="rounded"
+                                    height="2rem"
+                                    sx={{
+                                        width: '6rem',
+                                    }}
+                                />
+                            </Grid>
+                            <Grid>
+                                <Skeleton
+                                    variant="rounded"
+                                    height="2rem"
+                                    sx={{
+                                        width: '6rem',
+                                    }}
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid>
-                            <Skeleton
-                                variant="rounded"
-                                height="2rem"
-                                sx={{
-                                    width: '6rem',
-                                }}
-                            />
-                        </Grid>
-                    </Grid>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Grid>
     ))
 }
 
